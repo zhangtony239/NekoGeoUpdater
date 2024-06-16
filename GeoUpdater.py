39a86767aa0550a24d8e6b5c2ad17331a6da23e0
Zhang Tony <157202938+zhangtony239@users.noreply.github.com>
2024-06-16 12:11:18 +0800

    v1.3

--- GeoUpdater.py
+++ GeoUpdater.py
@@ -1,8 +1,8 @@
 import requests,os
 from tqdm import tqdm
 
-def get_latest():
-    response = requests.get('https://github.com/lyc8503/sing-box-rules/releases/latest/')
+def get_latest(base_url):
+    response = requests.get(base_url+'latest/')
     return response.url
 
 def getfile(url):
@@ -14,23 +14,42 @@ def getfile(url):
         print('HTTP ERROR:'+str(response.status_code))
         return None
 
-def savefile(file_content,filename):
+def savefile(filename):
     with open(filename,'wb') as file:
-        file.write(file_content)
+        file.write(getfile(base_url+filename))
         file.close()
     qbar.update(1)
 
+def core_check(lock):
+    if lock == '1':
+        print('核心锁定：sing-box')
+        return '.db','https://github.com/lyc8503/sing-box-rules/releases/'
+    elif lock == '0':
+        print('核心锁定：v2ray')
+        return '.dat','https://github.com/Loyalsoldier/v2ray-rules-dat/releases/'
+    else:
+        with open('config/groups/coreType','r') as file:
+            core_type = file.read()
+            file.close()
+        if core_type == '1':
+            print('当前核心：sing-box')
+            return '.db','https://github.com/lyc8503/sing-box-rules/releases/'
+        elif core_type == '0':
+            print('当前核心：v2ray')
+            return '.dat','https://github.com/Loyalsoldier/v2ray-rules-dat/releases/'
+        
+(suffix,base_url) = core_check(lock='1') #非官方版必须设置：锁sing-box，设为1；锁v2ray，设为0。官方版留空自动同步Nekoray内的内核设置。
 input('即将开始更新Geo文件，请确保网络连接正常，回车即可继续。')
 print('Geo文件更新开始，请勿中断程序……')
-if os.path.exists('geosite.db'):
-    os.remove('geosite.db')
-if os.path.exists('geoip.db'):
-    os.remove('geoip.db')
-latest_ver = get_latest().split('/')[-1:][0]
-base_url = 'https://github.com/lyc8503/sing-box-rules/releases/download/' + latest_ver + '/'
+if os.path.exists('geosite'+suffix):
+    os.remove('geosite'+suffix)
+if os.path.exists('geoip'+suffix):
+    os.remove('geoip'+suffix)
+latest_ver = get_latest(base_url).split('/')[-1:][0]
+base_url = base_url + 'download/' + latest_ver + '/'
 print('已获取最新版本：'+str(latest_ver))
 qbar = tqdm(total=2)
-geosite = savefile(getfile(base_url+'geosite.db'),'geosite.db')
-geoip = savefile(getfile(base_url+'geoip.db'),'geoip.db')
+geosite = savefile('geosite'+suffix)
+geoip = savefile('geoip'+suffix)
 input('更新完毕！回车即可关闭本窗口。')
 exit()
