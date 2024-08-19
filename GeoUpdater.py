d69912e25eb487a355c377602dca0d917a8d0e20
Zhang Tony <157202938+zhangtony239@users.noreply.github.com>
2024-08-19 12:20:24 +0800

    v1.7

--- GeoUpdater.py
+++ GeoUpdater.py
@@ -1,12 +1,22 @@
-import requests,shutil,os,json,time
+import requests,shutil,os,json,time,asyncio
 from tqdm import tqdm
 
-def download_file(filename):
+async def download_file(filename):
     url = base_url+filename
-    response = requests.get(url, proxies=proxies)
+    response = requests.get(url, proxies=proxies, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
     with open(filename+'.download', 'wb') as file:
-        file.write(response.content)
-    bar.update(1)
+        for data in response.iter_content(total_size//100):
+            file.write(data)
+            progress_bar.update(1)
+    file.close()
+    progress_bar.close()
+
+async def downloader():
+    await asyncio.gather(
+        download_file('geosite'+suffix),
+        download_file('geoip'+suffix)
+    )
 
 def init_proxies():
     with open('config/groups/nekobox.json') as file:
@@ -15,17 +25,20 @@ def init_proxies():
     proxies = {'http': 'http://127.0.0.1:'+port, 'https': 'http://127.0.0.1:'+port}
     return proxies
 
+def check_version():
+    response = requests.get(base_url, proxies=proxies)
+    return response.url.split('/')[-1]
+
 suffix,base_url = '.db','https://github.com/lyc8503/sing-box-rules/releases/latest/download/'
 proxies=init_proxies()
-print('正在获取最新Geo文件...')
 if os.path.exists('geosite'+suffix+'.download'):
     os.remove('geosite'+suffix+'.download')
 if os.path.exists('geoip'+suffix+'.download'):
     os.remove('geoip'+suffix+'.download')
-bar = tqdm(total=2)
 try:
-    geosite = download_file('geosite'+suffix)
-    geoip = download_file('geoip'+suffix)
+    print('正在获取最新Geo文件：'+check_version())
+    progress_bar = tqdm(total=101,bar_format='{percentage:3.0f}%|{bar}|')
+    asyncio.run(downloader())
 except Exception as Error:
     print('更新失败，请检查报错信息。')
     print('报错信息：'+str(Error))
