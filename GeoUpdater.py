fcd8019da8537eb183275bc6c5d8584e1e0c1083
Zhang Tony <157202938+zhangtony239@users.noreply.github.com>
2024-08-31 15:40:54 +0800

    v1.8

--- GeoUpdater.py
+++ GeoUpdater.py
@@ -1,7 +1,8 @@
-import requests,shutil,os,json,time,asyncio
+import requests,shutil,os,json,time
+from multiprocessing.pool import ThreadPool
 from tqdm import tqdm
 
-async def download_file(filename):
+def download_file(filename):
     url = base_url+filename
     response = requests.get(url, proxies=proxies, stream=True)
     total_size = int(response.headers.get('content-length', 0))
@@ -10,13 +11,6 @@ async def download_file(filename):
             file.write(data)
             progress_bar.update(1)
     file.close()
-    progress_bar.close()
-
-async def downloader():
-    await asyncio.gather(
-        download_file('geosite'+suffix),
-        download_file('geoip'+suffix)
-    )
 
 def init_proxies():
     with open('config/groups/nekobox.json') as file:
@@ -29,27 +23,29 @@ def check_version():
     response = requests.get(base_url, proxies=proxies)
     return response.url.split('/')[-1]
 
-suffix,base_url = '.db','https://github.com/lyc8503/sing-box-rules/releases/latest/download/'
+base_url = 'https://github.com/lyc8503/sing-box-rules/releases/latest/download/'
 proxies=init_proxies()
-if os.path.exists('geosite'+suffix+'.download'):
-    os.remove('geosite'+suffix+'.download')
-if os.path.exists('geoip'+suffix+'.download'):
-    os.remove('geoip'+suffix+'.download')
+if os.path.exists('geosite.db.download'):
+    os.remove('geosite.db.download')
+if os.path.exists('geoip.db.download'):
+    os.remove('geoip.db.download')
 try:
     print('正在获取最新Geo文件：'+check_version())
-    progress_bar = tqdm(total=101,bar_format='{percentage:3.0f}%|{bar}|')
-    asyncio.run(downloader())
+    progress_bar = tqdm(total=202,bar_format='{percentage:3.0f}%|{bar}|')
+    with ThreadPool(2) as pool:
+        pool.map(download_file, ['geosite.db', 'geoip.db'])
+    progress_bar.close()
 except Exception as Error:
     print('更新失败，请检查报错信息。')
     print('报错信息：'+str(Error))
     input('回车即可关闭本窗口。')
     exit()
-if os.path.exists('geosite'+suffix):
-    os.remove('geosite'+suffix)
-if os.path.exists('geoip'+suffix):
-    os.remove('geoip'+suffix)
-shutil.move('geosite'+suffix+'.download','geosite'+suffix)
-shutil.move('geoip'+suffix+'.download','geoip'+suffix)
+if os.path.exists('geosite.db'):
+    os.remove('geosite.db')
+if os.path.exists('geoip.db'):
+    os.remove('geoip.db')
+shutil.move('geosite.db.download','geosite.db')
+shutil.move('geoip.db.download','geoip.db')
 print('更新完毕，请重启Nekoray以应用更新。')
 time.sleep(1)
 exit()
